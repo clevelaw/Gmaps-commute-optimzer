b2f4db2fe8166bf20e0f384efff6c1550b191152
clevelaw <97043917+clevelaw@users.noreply.github.com>
2022-10-24 11:55:52 -0500

    Update commuter_optimizer.py

--- commuter_optimizer.py
+++ commuter_optimizer.py
@@ -1,7 +1,10 @@
 import googlemaps
 from datetime import datetime
+import time
+from weather_call import *
 
-my_date = datetime.today().strftime('%A')
+import json
+import csv
 
 #API KEY
 gmaps = googlemaps.Client(key='AIzaSyA76RjHZJgUN3RU2NPRbYTOHfy39IlvYg8')
@@ -10,52 +13,119 @@ gmaps = googlemaps.Client(key='AIzaSyA76RjHZJgUN3RU2NPRbYTOHfy39IlvYg8')
 work = "30.27776141307871, -97.73169690805364"
 home = "30.418150227932976, -97.65688514380827"
 
-#time that the commute was figured
-now = datetime.now()
-my_date = datetime.today().strftime('%A')
+# north dakota for debugging
+# work = "46.87405322078762, -96.79005193134536"
 
 class Commuting:
     "takes 2 address as coordinates and outputs a tuple with the (commute time from add1 to add2, add2 to add1)"
+
     def __init__(self, work_add, home_add):
         self._work_add = work_add
         self._home_add = home_add
+        self._now = datetime.now()
+
 
     def commute_time(self):
-        work_to_home = gmaps.directions(self._work_add, self._home_add, mode="driving", departure_time=now)
-        home_to_work = gmaps.directions(self._home_add, self._work_add, mode="driving", departure_time=now)
+        work_to_home = gmaps.directions(self._work_add, self._home_add, mode="driving", departure_time=self._now)
+        home_to_work = gmaps.directions(self._home_add, self._work_add, mode="driving", departure_time=self._now)
 
-        wth_commute_str = work_to_home[0]['legs'][0]['duration']['text']
-        htw_commute_str = home_to_work[0]['legs'][0]['duration']['text']
+        # commute time with no traffic in seconds
+        wth_no_traffic = work_to_home[0]['legs'][0]['duration']['value']
+        htw_no_traffic = work_to_home[0]['legs'][0]['duration']['value']
 
-        #time is returned as a string ie "99 min", filter and returns all ints
-        wth = [int(x) for x in wth_commute_str.split() if x.isdigit()][0]
-        htw = [int(x) for x in htw_commute_str.split() if x.isdigit()][0]
+        # gives the commute time in seconds, longer trips dont appear to have the traffic data
+        if "duration_in_traffic" not in work_to_home[0]['legs'][0].keys():
+            wth = wth_no_traffic
+        else:
+            wth = work_to_home[0]['legs'][0]['duration_in_traffic']['value']
+        if "duration_in_traffic" not in home_to_work[0]['legs'][0]:
+            htw = htw_no_traffic
+        else:
+            htw = home_to_work[0]['legs'][0]['duration_in_traffic']['value']
+
+        if wth < wth_no_traffic:
+            wth = wth_no_traffic
+        if htw < htw_no_traffic:
+            htw = htw_no_traffic
+
+
+        #warnings on the route
+        warnings_wth = work_to_home[0]['warnings']
+        warnings_htw = work_to_home[0]['warnings']
 
         date_data = datetime.now()
         day_of = datetime.today().strftime('%A')
 
+        s_from_midnight =(date_data.hour*3600) + (date_data.minute*60) + date_data.second
+
+        #finds the time for eight hours after the commute has been calculated
         eight_later = date_data.hour + 8
         if eight_later > 24:
             eight_later -= 24
 
-        times = [date_data.month, date_data.day, date_data.hour, date_data.minute, date_data.second, day_of, eight_later]
+        times = {
+            "to_work": htw,
+            "to_home": wth,
+            "to_work_no_traffic": htw_no_traffic,
+            "to_home_no_traffic": wth_no_traffic,
+            "warnings_wth": warnings_wth,
+            "warnings_htw": warnings_htw,
+            "year": date_data.year,
+            "month": date_data.month,
+            "day": date_data.day,
+            "hour": date_data.hour,
+            "minute": date_data.minute,
+            "second": date_data.second,
+            "day_of": day_of,
+            "eight_later": eight_later,
+            "s_from_midnight": s_from_midnight
+        }
+
+        return times
+
+    def show_output(self, dict):
+        print("-----------------------------------------------------------------")
+        print(f'Date: {dict["month"]}/{dict["day"]}, {dict["day_of"]}')
+        print(f'Time: {travel_times["hour"]}:{dict["minute"]}:{dict["second"]}')
+        print(f'Commute to work: {dict["to_work"] / 60: .3} minutes')
+        print(f'Commute to home: {dict["to_home"] / 60: .3} minutes')
+        print(f'Time lost to traffic to work: {(dict["to_work_no_traffic"] - dict["to_work"])/60: .3} minutes')
+        print(f'Time lost to traffic to home: {(dict["to_home_no_traffic"] - dict["to_home"])/60: .3} minutes')
+        print(f'Total day if leaving now: '
+              f'{dict["to_work"] / 3600 + dict["to_home"] / 3600 + 8 + 10 / 60: .3} hours')
+        print("-----------------------------------------------------------------")
+
+    def show_raw(self, dict):
+        print(dict.keys())
+        for i in dict.keys():
+            print(f"{i}:{dict[i]}")
 
 
+data_for_day = []
 
-        return wth, htw, times
+start_time = time.time()
+while True:
+    # running commute collection API
+    cc = Commuting(work, home)
+    travel_times = cc.commute_time()
+    cc.show_output(travel_times)
+    # cc.show_raw(travel_times)
 
-cc = Commuting(work, home)
-travel_times = cc.commute_time()
-#(to home, to work, [month, day, hour, minute, second, weekday, eight_later])
 
-time_dict = {}
-print("<<<<<<<<<<<<")
-print(travel_times[2][5])
-print("Month:", travel_times[2][0], "Day:", travel_times[2][1])
-print("Time:", travel_times[2][2],":",travel_times[2][3],":",travel_times[2][4])
-print("Time 8 hours from now:", travel_times[2][6],":",now.minute,":",now.second)
-print("Commute to work:", travel_times[1])
-print("Commute to home:", travel_times[0])
-print("total day at this rate:", travel_times[0]/60+travel_times[1]/60+8+10/60, "hours")
+    # running weather collection API
+    ww = Commute_weather("Austin")
+    whats_weather = ww.gather_weather()
+    ww.show_weather(whats_weather)
+    data_for_day.append([travel_times, whats_weather])
 
-#time_dict[now] = [wth_commute, htw_commute]
+    save_file = "test" + str(travel_times["month"]) + "-" + str(travel_times["day"]) + "commute.json"
+
+    print("ahsdfasdfasdf")
+    with open(save_file, "w") as write_file:
+        json.dump(data_for_day, write_file)
+
+    print("waiting...")
+    #run program every 1 minute
+    time.sleep(10.0-((time.time() - start_time)%10.0))
+
+
